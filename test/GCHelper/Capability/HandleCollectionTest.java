99bb1057cd25682a98c16125f4590cd39ba14c80
Jose Sebastian Battig <jsbattig@convey.com>
2016-11-08 10:56:12 -0600

    B-0 file renamed

--- test/GCHelper/Capability/HandleCollectionTest.java
+++ test/GCHelper/Capability/HandleCollectionTest.java
@@ -1,44 +1,42 @@
 package GCHelper.Capability;
 
-import GCHelper.Capability.ConcurrentDependencies;
-import GCHelper.Capability.HandleContainer;
+import GCHelper.Capability.HandleCollection;
 import org.junit.Test;
 import org.junit.Before;
 
 import static org.junit.Assert.*;
 
-public class ConcurrentDependenciesTest {
-    private ConcurrentDependencies<String, Integer> _concurrentDependencies;
-
+public class HandleCollectionTest {
+    private HandleCollection<String, Integer> _handles;
     @Before
     public void Setup()
     {
-        _concurrentDependencies = new ConcurrentDependencies<String, Integer>();
+        _handles = new HandleCollection<String, Integer>();
     }
 
     @Test
     public void add() throws Exception {
-        assertTrue(_concurrentDependencies.Add("Hello", 1));
-        assertFalse(_concurrentDependencies.Add("Hello", 1));
+        assertTrue(_handles.Add("Hello", 1));
+        assertFalse(_handles.Add("Hello", 1));
     }
 
     @Test
     public void remove() throws Exception {
-        assertFalse(_concurrentDependencies.Remove("Hello", 1));
-        assertTrue(_concurrentDependencies.Add("Hello", 1));
-        assertTrue(_concurrentDependencies.Remove("Hello", 1));
-        assertFalse(_concurrentDependencies.Remove("Hello", 1));
+        assertFalse(_handles.Remove("Hello", 1));
+        assertTrue(_handles.Add("Hello", 1));
+        assertTrue(_handles.Remove("Hello", 1));
+        assertFalse(_handles.Remove("Hello", 1));
     }
 
     @Test
     public void iterator() throws Exception {
         Integer counter = 0;
-        for(HandleContainer<String, Integer> handle : _concurrentDependencies)
+        for(HandleContainer<String, Integer> handle : _handles)
             counter++;
         assertEquals((Integer)0, counter);
-        _concurrentDependencies.Add("Hello", 1);
-        _concurrentDependencies.Add("Hello 2", 1);
-        for(HandleContainer<String, Integer> handle : _concurrentDependencies)
+        _handles.Add("Hello", 1);
+        _handles.Add("Hello 2", 1);
+        for(HandleContainer<String, Integer> handle : _handles)
             counter++;
         assertEquals((Integer)2, counter);
     }
